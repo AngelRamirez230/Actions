6344471e412d8688edb8cf7e8c99d642c52006f5
AngelRamirez230 <angelramirezperez24@gmail.com>
2024-12-11 19:27:32 -0600

    bbbbb

--- django/CRUD/CRUD/settings.py
+++ django/CRUD/CRUD/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'crud_nuevo',
         'USER': 'root',
         'PASSWORD': 'Adavid02',
-        'HOST': 'localhost',
+        'HOST': 'mysql',
         'PORT': '3306',
     }
 }
